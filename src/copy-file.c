e07ec76a10a38762310a4447decffa556a53fe6f
sebarro04 <sebastianarroniz55@gmail.com>
2024-04-15 06:46:13 -0600

    Change copy method

--- src/copy-file.c
+++ src/copy-file.c
@@ -5,47 +5,39 @@
 
 void copy_file(char *filename, char *dirname)
 {
-    FILE *file = fopen(filename, "r");
+    FILE *file = fopen(filename, "rb");
     if (file == NULL)
     {
         printf("Error opening the file %s to read the file content\n", filename);
         exit(1);
     }
 
-    printf("After fopen\n");
-
-    fseek(file, 0, SEEK_END);
-    long file_size = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    printf("file size: %ld\n", file_size);
-
-    char *file_content = malloc(file_size + 1);
-    fread(file_content, file_size, 1, file);
-
-    fclose(file);
-
-    printf("After read\n");
-
     char *temp_filename = basename(filename);
 
-    char *new_filename = malloc(sizeof(temp_filename) + sizeof(dirname) + 1);
+    char *new_filename = malloc(sizeof(temp_filename) + sizeof(dirname));
     strcat(new_filename, dirname);
     strcat(new_filename, temp_filename);
 
     printf("New filename: %s\n", new_filename);
 
-    FILE *new_file = fopen(new_filename, "w");
+    FILE *new_file = fopen(new_filename, "wb");
     if (new_file == NULL)
     {
         printf("Error creating the file %s to write the file content\n", new_filename);
         exit(1);
     }
-    int result = fwrite(file_content, file_size, 1, new_file);
 
+    char buffer[255];
+
+    while (!feof(file))
+    {
+        fread(buffer, sizeof(buffer), 1, file);
+        fwrite(buffer, sizeof(buffer), 1, new_file);
+    }
+
+    fclose(file);
     fclose(new_file);
 
-    free(file_content);
     free(new_filename);
 
     return;
--- src/main.c
+++ src/main.c
@@ -9,11 +9,11 @@ int main()
     char cwd[PATH_MAX];
     printf("CWD: %s\n", getcwd(cwd, sizeof(cwd)));
 
-    // copy_file("../test/test-01.txt", "../copy-test/");
-    // printf("File 1 copied\n");
+    copy_file("../test/test-01.txt", "../copy-test/");
+    printf("File 1 copied\n");
 
-    copy_file("../test/test-02.txt", "../copy-test/");
-    printf("File 2 copied\n");
+    // copy_file("../test/test-02.txt", "../copy-test/");
+    // printf("File 2 copied\n");
 
     // copy_file("../test/test-03.txt", "../copy-test/");
     // printf("File 3 copied\n");
