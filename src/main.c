da5911e74b2a11f843f09ccfe9ad4e2676643586
sebarro04 <sebastianarroniz55@gmail.com>
2024-04-15 21:24:54 -0600

    Update main

--- src/main.c
+++ src/main.c
@@ -3,20 +3,24 @@
 #include <stdio.h>
 #include <linux/limits.h>
 #include "copy-file.h"
+#include "utils.h"
 
 int main()
 {
     char cwd[PATH_MAX];
     printf("CWD: %s\n", getcwd(cwd, sizeof(cwd)));
 
-    copy_file("../test/files/test-01.txt", "../test/copy/test-01.txt");
-    printf("File 1 copied\n");
+    char *new_filename = get_new_filename("test01/test/files/test-01.txt", "test/copy");
+    free(new_filename);
 
-    copy_file("../test/files/test-02.txt", "../test/copy/test-02.txt");
-    printf("File 2 copied\n");
+    // copy_file("../test/files/test-01.txt", "../test/copy/test-01.txt");
+    // printf("File 1 copied\n");
 
-    copy_file("../test/files/test-03.txt", "../test/copy/test-03.txt");
-    printf("File 3 copied\n");
+    // copy_file("../test/files/test-02.txt", "../test/copy/test-02.txt");
+    // printf("File 2 copied\n");
+
+    // copy_file("../test/files/test-03.txt", "../test/copy/test-03.txt");
+    // printf("File 3 copied\n");
 
     return 0;
 }
