a5b8b8bb3360b9afa3a541653ea444f635127a0b
sebarro04 <sebastianarroniz55@gmail.com>
2024-04-15 18:21:40 -0600

    Update main

--- src/main.c
+++ src/main.c
@@ -9,14 +9,14 @@ int main()
     char cwd[PATH_MAX];
     printf("CWD: %s\n", getcwd(cwd, sizeof(cwd)));
 
-    copy_file("../test/test-01.txt", "../copy-test/");
+    copy_file("../test/files/test-01.txt", "../test/copy/test-01.txt");
     printf("File 1 copied\n");
 
-    // copy_file("../test/test-02.txt", "../copy-test/");
-    // printf("File 2 copied\n");
+    copy_file("../test/files/test-02.txt", "../test/copy/test-02.txt");
+    printf("File 2 copied\n");
 
-    // copy_file("../test/test-03.txt", "../copy-test/");
-    // printf("File 3 copied\n");
+    copy_file("../test/files/test-03.txt", "../test/copy/test-03.txt");
+    printf("File 3 copied\n");
 
     return 0;
 }
