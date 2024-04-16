e78ebfd0c5b26a034d6e3d9ab01e52c2b4759bbc
sebarro04 <sebastianarroniz55@gmail.com>
2024-04-15 22:17:53 -0600

    Update compile script

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 mkdir -p bin
 gcc -o bin/main.o -c src/main.c
-gcc -o bin/copy-file.o -c src/copy-file.c
 gcc -o bin/utils.o -c src/utils.c
+gcc -o bin/files-list.o -c src/files-list.c
 
 cd bin
-gcc -o main.out main.o copy-file.o utils.o
+gcc -o main.out main.o utils.o files-list.o
