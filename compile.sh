9cbd1b8dfe1ac31505c30833b7d31d603a63ba67
sebarro04 <sebastianarroniz55@gmail.com>
2024-04-17 03:57:41 -0600

    Update compile script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 mkdir -p bin
-gcc -o bin/main.o -c src/main.c
-gcc -o bin/utils.o -c src/utils.c
+gcc -o bin/main.o -c src/code/main.c
+gcc -o bin/archives.o -c src/code/archives.c
+gcc -o bin/msg-queue.o -c src/code/msg-queue.c
 
 cd bin
-gcc -o main.out main.o utils.o
+gcc -o main.out main.o archives.o msg-queue.o
